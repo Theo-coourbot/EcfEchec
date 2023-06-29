b3a302e0e80c57f8b3c372cb57bdd4565f8f1bd9
theo courbot <michelle.marco@gmail.com>
2023-06-29 08:59:16 +0200

    creation du projet

---
+++ demo/src/main/java/com/todo/demo/ECFSpringApplication.java
@@ -0,0 +1,13 @@
+package com.todo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECFSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECFSpringApplication.class, args);
+	}
+
+}
---
+++ demo/src/main/java/com/todo/demo/entity/Tournoi.java
@@ -0,0 +1,16 @@
+package com.todo.demo.entity;
+
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tournoi {
+}
